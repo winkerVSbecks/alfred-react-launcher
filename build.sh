a0599d425a721b0779067fed5165b28e5748e45c
Varun Vachhar <varunvachhar@gmail.com>
2016-08-20 17:37:40 -0400

    added a build script

--- build.sh
+++ build.sh
@@ -13,5 +13,8 @@ cp -r ./toc.js $OUTPUT
 cp -r ./info.plist $OUTPUT
 cp -r ./D38BEEEE-2056-47E6-88AC-7D0C768B5E4E.png $OUTPUT
 cp -r ./icon.png $OUTPUT
-zip -r react-launcher.alfredworkflow $OUTPUT
+cp -r ./README.md $OUTPUT
+cd $OUTPUT
+zip -r ../react-launcher.alfredworkflow .
+cd ..
 rm -rf $OUTPUT
